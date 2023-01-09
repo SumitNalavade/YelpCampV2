637f8f1edda2e0e27209ff0641b4d8c16176542b
SumitNalavade <vs.nalavade2003@gmail.com>
2023-01-09 16:27:48 -0600

    Sign in with popup

--- Components/Navbar/SignedOutNav.tsx
+++ Components/Navbar/SignedOutNav.tsx
@@ -1,8 +1,33 @@
-import { signIn } from "next-auth/react";
-
 const SignedOutNav: React.FC = () => {
+  const popupCenter = (url: string, title: string) => {
+    const dualScreenLeft = window.screenLeft ?? window.screenX;
+    const dualScreenTop = window.screenTop ?? window.screenY;
+
+    const width =
+      window.innerWidth ?? document.documentElement.clientWidth ?? screen.width;
+
+    const height =
+      window.innerHeight ??
+      document.documentElement.clientHeight ??
+      screen.height;
+
+    const systemZoom = width / window.screen.availWidth;
+
+    const left = (width - 500) / 2 / systemZoom + dualScreenLeft;
+    const top = (height - 550) / 2 / systemZoom + dualScreenTop;
+
+    const newWindow = window.open(
+      url,
+      title,
+      `width=${500 / systemZoom},height=${550 / systemZoom
+      },top=${top},left=${left}`
+    );
+
+    newWindow?.focus();
+  };
+
   return (
-    <div className="avatar cursor-pointer" tabIndex={0} onClick={() => signIn()}>
+    <div className="avatar cursor-pointer" tabIndex={0} onClick={() => popupCenter("/signIn", "Sample Sign In")}>
       <div className="w-8 rounded-full">
         <img src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541" />
       </div>
